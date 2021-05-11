f03733109b41f8189467caec1f0edae695e4106f
Chetan <chetankashetty@gmail.com>
2021-05-11 13:35:16 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,6 +1,14 @@
 param(
     [Parameter(Mandatory = $false)]
-    [Boolean] $isProduction
+    [Boolean] $isProduction = $false
 )
 Write-Output $isProduction
 Write-Output "Hey, I am in poweshell script!"
+if ($isProduction)
+{
+    Write-Host "I am in true condn"
+}
+else {
+    Write-Host "I am in false condn"
+    }
+    
